9a01bf45bab3f19409aa74b401e46f8506acef99
Papoune <npapon@live.fr>
2021-01-11 16:43:08 +0100

    first commit

---
+++ ticket-batch/src/main/java/fr/papounworld/batch/App.java
@@ -0,0 +1,13 @@
+package fr.papounworld.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ticket-business/src/main/java/fr/papounworld/business/App.java
@@ -0,0 +1,13 @@
+package fr.papounworld.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
