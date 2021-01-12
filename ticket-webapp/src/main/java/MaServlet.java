ab711b026f21106596f8219b69969a8c0e7cca6c
Papoune <npapon@live.fr>
2021-01-12 17:39:10 +0100

    dependances

---
+++ ticket-webapp/src/main/java/MaServlet.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MaServlet implements Servlet {
+
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
